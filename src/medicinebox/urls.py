043bffe6c1f740ce2c1cca0c3a6663ec4def3690
AlexGalPar14 <80201646+AlexGalPar14@users.noreply.github.com>
2022-12-14 16:01:00 +0100

    Urls global

--- src/medicinebox/urls.py
+++ src/medicinebox/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from pages.views import home_view
+from django.urls import include, path
 
 urlpatterns = [
-    path('', home_view, name='home'),
+    path('', include('pages.urls')),
     path('admin/', admin.site.urls),
 ]
