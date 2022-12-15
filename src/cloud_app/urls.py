6ba15838bef2426836321f79132514f7c5016f7a
AlexGalPar14 <80201646+AlexGalPar14@users.noreply.github.com>
2022-12-15 22:03:04 +0100

    Canvi d'imatges

--- src/cloud_app/urls.py
+++ src/cloud_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_view, name='home'),
+    path('', views.home_view, name='home_page'),
+    path('', views.log_in_view, name='log_in'),
+    path('', views.user_patient_view, name='home_page'),
 ]
--- src/cloud_app/views.py
+++ src/cloud_app/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
 def home_view(request, *args, **kwargs):
-    # return HttpResponse('<h1>Hello World</h1>')
-    return render(request, 'home.html', {})
+    return render(request, 'home_page.html', {})
+
+def log_in_view(request, *args, **kwargs):
+    return render(request, 'log_in.html', {})
+
+def user_patient_view(request, *args, **kwargs):
+    return render(request, 'user_patient.html', {})
